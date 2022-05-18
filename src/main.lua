4341df7892787bc90405e4ce938ee5bb91b3482f
Josh Perry <joshperry@outlook.com>
2022-05-18 22:18:56 +0100

    Example: Map scrolling

--- src/main.lua
+++ src/main.lua
@@ -1,11 +1,15 @@
 local ldtk = require("ldtk")
 local map
 
+local mx, my = 0, 0
+local scrollX, scrollY = 0, 0
+
 function love.load()
     map = ldtk.map("test.ldtk")
 end
 
 function love.draw()
+    love.graphics.translate(math.floor(scrollX), math.floor(scrollY))
     map:draw()
 end
 
@@ -17,4 +21,13 @@ function love.keypressed(key, scancode)
     if scancode == "escape" then
         love.event.quit()
     end
+end
+
+function love.mousemoved(x, y, dx, dy)
+    if not love.mouse.isDown(1) then
+        return
+    end
+
+    scrollX = scrollX + dx
+    scrollY = scrollY + dy
 end
